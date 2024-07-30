5095a8ee7e3cd766c90949f0db8ef5a5b32ae0bc
vinhtran202 <vinhtran.gtvt@gmail.com>
2024-07-30 09:43:48 +0700

    install next js and create the first tmplate

---
+++ src/app/head.jsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>Google Clone</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="Google clone created by Next JS 14" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
